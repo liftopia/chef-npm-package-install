478e05c5a0a335e1a3c8f90fee2206fee39145be
Aaron Daniel <amdtech@me.com>
2014-07-07 15:05:19 -0700

    Add a name field for berkshelf 3

--- metadata.rb
+++ metadata.rb
@@ -26,5 +26,6 @@ license          "Apache License, Version 2.0"
 description      "Installs npm packages"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+name             "npm"
 
 depends "nodejs"
